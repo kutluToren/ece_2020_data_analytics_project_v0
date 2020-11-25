d999e4f19545839c53c336a71a4765dbdae6739b
kutluToren <55382539+kutluToren@users.noreply.github.com>
2020-11-25 15:45:53 +0100

    Update server.R

--- server.R
+++ server.R
@@ -6,8 +6,19 @@ source("helper.R")
 shinyServer(function(input, output) {
   
   
+  #display selection
+  output$selections <- renderUI(
+    paste("Your city selection is",
+          input$city_compare1,"in date", 
+          find_data_dates(input$city_compare1,input$date_a1_city1),
+          "and",input$city_compare2,"in date",
+          find_data_dates(input$city_compare2,input$date_a1_city2),
+          sep= " ")
+  )
+  
+  
   # render the map by selected city and date (date recovered from database)
   output$mymap <- renderLeaflet({create_leaflet(input$city_select,
                                                 find_data_dates(input$city_select,input$date_select)) })
- 
+
   })
