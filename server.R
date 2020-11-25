cfb70e1145dd6f308adbd3af9c699a09affe6738
kutluToren <55382539+kutluToren@users.noreply.github.com>
2020-11-25 04:36:07 +0100

    Update server.R

--- server.R
+++ server.R
@@ -6,7 +6,6 @@ source("helper.R")
 shinyServer(function(input, output) {
   
   
-  output$info_box <- renderUI(as.numeric(input$date_select))
   # render the map by selected city and date (date recovered from database)
   output$mymap <- renderLeaflet({create_leaflet(input$city_select,
                                                 find_data_dates(input$city_select,input$date_select)) })
