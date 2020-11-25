98579318f78e03da0236c09f0e796c1526c39fdd
kutluToren <55382539+kutluToren@users.noreply.github.com>
2020-11-25 02:45:22 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,5 +1,16 @@
 library(shiny)
 
+#to get the functions available in helper.R, Check ReadMe for detail
+source("helper.R")
+
 shinyServer(function(input, output) {
   
+
+  # render the map by selected city and date
+  output$mymap <- renderLeaflet({
+    create_leaflet(
+                  unlist(strsplit(input$city_date_select,"@"))[1],
+                  unlist(strsplit(input$city_date_select,"@"))[2])
+  })
+
 })
