992f3eb92712c8090192bffd3d70d63e9215756b
kutluToren <55382539+kutluToren@users.noreply.github.com>
2020-11-26 01:11:37 +0100

    updated last tab

--- server.R
+++ server.R
@@ -13,6 +13,7 @@ shinyServer(function(input, output) {
           find_data_dates(input$city_compare1,input$date_a1_city1),
           "and",input$city_compare2,"in date",
           find_data_dates(input$city_compare2,input$date_a1_city2),
+          input$feature_select,
           sep= " ")
   )
   
@@ -21,12 +22,24 @@ shinyServer(function(input, output) {
   output$mymap <- renderLeaflet({create_leaflet(input$city_select,
                                                 find_data_dates(input$city_select,input$date_select)) })
   
-  #render_plot
+  
+  #render plot for tab1
   output$analysis1 <- renderPlot(
     {analysis1(input$city_compare1,
                input$city_compare2, 
                find_data_dates(input$city_compare1,input$date_a1_city1), 
-               find_data_dates(input$city_compare2,input$date_a1_city2) 
+               find_data_dates(input$city_compare2,input$date_a1_city2),
+               input$feature_select_a2,
+               input$plot_select_a1
     )})
+  
+  #render plot for tab2
+  output$analysis2 <- renderPlot(
+    {analysis2(input$city_select2,
+               find_data_dates(input$city_select2,input$date_select2),
+               input$feature_select_a2
+               )
+    }
+  )
 
   })
